cc7c97c75ae391ba3ad378c86a2eb79862e8cbb0
cypox <am_neggaz@esi.dz>
2021-04-02 21:54:21 +0200

    added test file

---
+++ bot/bot_main.py
@@ -0,0 +1,26 @@
+from datasource import datafeed
+from dataset import dataset
+from forecast import predictor
+from plot_utils import show_plot
+
+import random
+
+load_saved = True
+p = predictor()
+if load_saved:
+  p.load_model('trained.h5')
+else:
+  p.build_model()
+  f = datafeed()
+  f.update_ticker('NIO')
+  d = dataset()
+  d.build_dataset(f.get_dataframe())
+  p.train_model(d)
+  p.save_model('trained.h5')
+
+input_seq = [random.random() * 5 + 30 for i in range(30)]
+expected_seq = random.random() * 5 + 30
+output_seq = p.predict_single(input_seq)
+
+plt = show_plot([input_seq, expected_seq, output_seq], 0, 'Sample Example')
+plt.savefig('prediction_output.png')
--- bot/forecast.py
+++ bot/forecast.py
@@ -46,25 +46,3 @@ class predictor:
   
   def load_model(self, path):
     self.model = keras.models.load_model(path)
-
-load_saved = True
-p = predictor()
-if load_saved:
-  p.load_model('trained.h5')
-else:
-  p.build_model()
-  f = datafeed()
-  f.update_ticker('NIO')
-  d = dataset()
-  d.build_dataset(f.get_dataframe())
-  p.train_model(d)
-  p.save_model('trained.h5')
-
-import random
-input_seq = [random.random() * 5 + 30 for i in range(30)]
-expected_seq = random.random() * 5 + 30
-output_seq = p.predict_single(input_seq)
-
-from plot_utils import show_plot
-plt = show_plot([input_seq, expected_seq, output_seq], 0, 'Sample Example')
-plt.savefig('prediction_output.png')
